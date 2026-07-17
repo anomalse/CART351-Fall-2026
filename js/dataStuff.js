6d66a1da2e962f8122567eeabd86626f2af1ca4c
Sabine Rosenberg <11468543+anomalse@users.noreply.github.com>
2026-07-16 21:10:34 -0400

    init

---
+++ js/dataStuff.js
@@ -0,0 +1,24 @@
+ 
+ async function loadTitleHeader(){
+
+    let elContainer = document.querySelector("#title-head");
+  
+    elContainer.textContent = 'CART 351 FALL 2026';
+}
+
+ async function loadNav(){
+
+    let elContainer = document.querySelector("#cart351Nav");
+    let response = await fetch('nav.html')
+    let htmlNav = await response.text();
+    elContainer.innerHTML =htmlNav;
+}
+
+function loadTitle(){
+let title = document.querySelector("title");
+title.textContent = 'CART 351 FALL 2026';
+}
+
+loadTitle();
+loadTitleHeader();
+loadNav();
